323a7b4f051e06b6b40a53247f348b35d0dcbdcb
vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-01-15 22:30:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function threeSum(arr, target) {
+ function threeSum(arr, target) {
   //your code here
+  let ans = Infinity;
+  arr.sort((a, b) => a - b);
+  arr.forEach((a, i) => {
+    const target2 = target - a;
+    let l = i + 1;
+    let r = arr.length - 1;
+    while (l < r) {
+      if (Math.abs(target - (arr[l] + arr[r] + a)) <= Math.abs(target - ans)) {
+        ans = arr[l] + arr[r] + a;
+      }
+      if (arr[l] + arr[r] > target2) r--;
+      else l++;
+    }
+  });
+  return ans;
 }
-
+ 
 module.exports = threeSum;
